3f56c92c1f7811411e78696098f77cea3ecc139f
EK <49726649+EliEl7777@users.noreply.github.com>
2024-05-11 14:07:27 +0300

    db.php

--- privateapitrussiv_html/db.php
+++ privateapitrussiv_html/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$db_name = "equi-264607:us-central1:trussivmysql";
-$db_server = "35.184.61.149";
-$db_user = "p1002810957360-v0la13@gcp-sa-cloud-sql.iam.gserviceaccount.com";
-$db_pass = "Sea0710579";
+$db_name = "";
+$db_server = "";
+$db_user = "";
+$db_pass = 
 
 $db = new PDO("mysql:host={$db_server};dbname={$db_name};charset=utf8", $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
